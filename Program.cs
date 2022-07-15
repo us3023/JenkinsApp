3cb208eb8d74d01030990814a796f7398aa38bcc
Shanu <us3023@srmist.edu.in>
2022-07-15 15:49:02 +0530

    added dll file

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using dll_add_multiply;
 namespace JenkinsApp
 {
     class Program
@@ -9,6 +9,14 @@ namespace JenkinsApp
             Console.WriteLine("Hello World!");
             Console.WriteLine("Welcome to Jenkins");
             Console.ReadLine();
+            Add file = new Add();
+            Console.WriteLine(file.Add_(4,5));
+            
+            
+
+
+            
+
             
         }
     }
