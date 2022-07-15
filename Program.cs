999672c35274f902432505ce18c927b10c001a96
Shanu <us3023@srmist.edu.in>
2022-07-15 11:50:33 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace JenkinsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Jenkins");
+            Console.ReadLine();
+            
+        }
+    }
+}
